a8919b5756f8c25420563ffa27a87d404dd363ba
Karen Munoz <karen00munoz@gmail.com>
2022-10-27 21:56:06 +0200

    Add more configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 export default defineNuxtConfig({
-  css: ["~/assets/css/tailwind.css"],
+  css: [
+    "~/assets/fonts/XL-9XL/stylesheet.css",
+    "~/assets/fonts/XS-LG/stylesheet.css",
+    "~/assets/css/icomoon.css",
+    "~/assets/css/tailwind.css",
+  ],
   build: {
     postcss: {
       postcssOptions: require("./postcss.config.js"),
