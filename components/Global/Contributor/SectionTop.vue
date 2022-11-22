b7875a043caf3b5a2f0143c12912718794cf7a66
Karen Munoz <karen00munoz@gmail.com>
2022-11-22 15:55:34 +0200

    Fix for top contributor max width

--- components/Global/Contributor/SectionTop.vue
+++ components/Global/Contributor/SectionTop.vue
@@ -15,37 +15,39 @@ async function showAll() {
 
 <template>
   <div id="top_contributors" class="bg-tertiary-500 dark:bg-tertiary-800">
-    <UIContainer class="!max-w-[1080px] py-20">
-      <div class="flex items-center justify-between">
-        <UIHeading :level="5" class="text-[30px] !text-white"> Follow our top contributors </UIHeading>
+    <UIContainer class="py-20">
+      <div class="mx-auto !max-w-[1080px]">
+        <div class="flex items-center justify-between">
+          <UIHeading :level="5" class="text-[30px] !text-white"> Follow our top contributors </UIHeading>
 
-        <NuxtLink to="#" class="text-sm font-semibold text-white"> Show All </NuxtLink>
-      </div>
+          <NuxtLink to="#" class="text-sm font-semibold text-white"> Show All </NuxtLink>
+        </div>
 
-      <div class="mt-[53px]">
-        <Carousel
-          :items-to-show="1"
-          snap-align="center"
-          :breakpoints="{
-            480: { itemsToShow: 2, snapAlign: 'start' },
-            768: { itemsToShow: 3, snapAlign: 'start' },
-            1152: { itemsToShow: 4, snapAlign: 'start' },
-          }">
-          <Slide v-for="(contributor, key) in contributorsData" :key="key" class="xl:!px-[7.5px]">
-            <GlobalContributorCard
-              :name="contributor.data.name"
-              :handle="contributor.data.handle"
-              :user-image="contributor.data.user_image"
-              :total-followers="contributor.data.total_followers"
-              :total-books="contributor.data.total_books"
-              :total-clips="contributor.data.total_clips"
-              :following="false" />
-          </Slide>
+        <div class="mt-[53px]">
+          <Carousel
+            :items-to-show="1"
+            snap-align="center"
+            :breakpoints="{
+              480: { itemsToShow: 2, snapAlign: 'start' },
+              768: { itemsToShow: 3, snapAlign: 'start' },
+              1152: { itemsToShow: 4, snapAlign: 'start' },
+            }">
+            <Slide v-for="(contributor, key) in contributorsData" :key="key" class="xl:!px-[7.5px]">
+              <GlobalContributorCard
+                :name="contributor.data.name"
+                :handle="contributor.data.handle"
+                :user-image="contributor.data.user_image"
+                :total-followers="contributor.data.total_followers"
+                :total-books="contributor.data.total_books"
+                :total-clips="contributor.data.total_clips"
+                :following="false" />
+            </Slide>
 
-          <template #addons>
-            <Pagination class="!mt-[53px]" />
-          </template>
-        </Carousel>
+            <template #addons>
+              <Pagination class="!mt-[53px]" />
+            </template>
+          </Carousel>
+        </div>
       </div>
     </UIContainer>
   </div>
