7d8edb260fe71606231d56e4f9320327015ac85d
Karen Munoz <karen00munoz@gmail.com>
2022-11-30 13:41:06 +0000

    Complete task #33

--- pages/haggadahs/index.vue
+++ pages/haggadahs/index.vue
@@ -1,3 +1,36 @@
 <template>
-  <h1>Route component, not implemented yet</h1>
+  <div>
+    <div class="pt-[72px]">
+      <UIContainer class="flex flex-col items-center">
+        <UIIcon icon="icon-book-f" shape="square" class="bg-gradient2 shadow-md" />
+        <UIHeading :level="1" class="mt-5 text-center text-[3rem]">Explore Passover Haggadahs</UIHeading>
+        <UISearch
+          rules="required"
+          redirect-address="/search-haggadahs"
+          query-key="haggadah"
+          placeholder="Search Haggadahs by keyword or topic"
+          class="mt-[25px]" />
+        <div class="mt-[45px] text-center">
+          <span class="text-sm leading-none text-gray-900"> Popular categories </span>
+          <div class="mt-[22px] flex flex-wrap justify-center space-x-1.5">
+            <UIBadge
+              v-for="category in categories"
+              :key="category"
+              size="md"
+              type="accent-yellow"
+              class="mb-1.5 md:mb-0">
+              {{ category }}
+            </UIBadge>
+          </div>
+        </div>
+        <!-- Temporarily -->
+        <div class="mt-[90px]" />
+      </UIContainer>
+    </div>
+  </div>
 </template>
+
+<script lang="ts" setup>
+import { ref, Ref } from 'vue';
+const categories: Ref<string[]> = ref(['Family', 'Comedy', 'Pop Culture', 'Ukraine', 'Humanity', 'Kids']);
+</script>
