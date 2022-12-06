e48726843b86283054f5f486f1cae027bcfa9829
Karen Munoz <karen00munoz@gmail.com>
2022-12-06 13:17:45 +0500

    complete task #96

--- pages/explore-haggadahs/preview.vue
+++ pages/explore-haggadahs/preview.vue
@@ -18,9 +18,69 @@
             <UIButton class="mr-4 w-full bg-accent-yellow-500 text-gray-900 hover:bg-accent-yellow-600">
               Follow Contributor
             </UIButton>
-            <UIButton color="light" class="w-full border bg-transparent text-white hover:bg-white hover:text-gray-900">
+            <UIButton
+              color="light"
+              class="w-full border !bg-transparent text-white hover:!bg-white hover:text-gray-900">
               View Profile
             </UIButton>
+          </div>
+        </div>
+      </UIContainer>
+    </div>
+    <div class="pt-[100px]">
+      <UIContainer>
+        <div class="flex items-center justify-between">
+          <UIHeading :level="3" class="!text-3xl">
+            Other Haggadahs by <span class="text-secondary-500">Esther Kustanowitz</span>
+          </UIHeading>
+          <UIButton class="!p-0 !text-sm !leading-[21px] text-gray-700">Show all</UIButton>
+        </div>
+        <div class="mt-[61px]">
+          <div class="grid grid-cols-1 place-items-center gap-8 md:grid-cols-2 lg:grid-cols-3">
+            <GlobalHaggadahCard
+              v-for="item in haggadahs"
+              :key="item"
+              route="#"
+              :img-src="haggadahsCardImage"
+              title="10 Minute Dayenu Seder"
+              :slug="`haddahs-${item}`"
+              text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet accusamus sit rem officia. Sit aperiam, tempora iste ab porro hic ratione consequatur a illum harum voluptate optio! Alias, nihil sapiente."
+              :read-time="10"
+              contributor-name="Haggadahs"
+              contributor-initials="HD"
+              :contributor-avatar="null"
+              :topic-tags="['Trending', 'Humanity']"
+              :is-added-to-bookmark="false"
+              :is-owner="false"
+              :is-liked="false" />
+          </div>
+        </div>
+      </UIContainer>
+    </div>
+    <div class="pt-[83px] pb-[100px]">
+      <UIContainer>
+        <div class="flex items-center justify-between">
+          <UIHeading :level="4" class="!text-[26px] !leading-[31px]">Similar Topic Related Haggadahs</UIHeading>
+          <UIButton class="!p-0 !text-sm !leading-[21px] text-gray-700">Show all</UIButton>
+        </div>
+        <div class="mt-[34px]">
+          <div class="grid grid-cols-1 place-items-center gap-8 md:grid-cols-1 lg:grid-cols-3">
+            <GlobalHaggadahCard
+              v-for="item in haggadahs"
+              :key="item"
+              route="#"
+              :img-src="haggadahsCardImage"
+              title="10 Minute Dayenu Seder"
+              :slug="`haddahs-${item}`"
+              text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet accusamus sit rem officia. Sit aperiam, tempora iste ab porro hic ratione consequatur a illum harum voluptate optio! Alias, nihil sapiente."
+              :read-time="10"
+              contributor-name="Haggadahs"
+              contributor-initials="HD"
+              :contributor-avatar="null"
+              :topic-tags="['Trending', 'Humanity']"
+              :is-added-to-bookmark="false"
+              :is-owner="false"
+              :is-liked="false" />
           </div>
         </div>
       </UIContainer>
@@ -29,5 +89,7 @@
 </template>
 
 <script lang="ts" setup>
+const haggadahs = [...Array(3).keys()];
 const Avatar = (await import('@/assets/images/avatar.png')).default;
+const haggadahsCardImage = (await import('@/assets/images/haggadah-card-image.png')).default;
 </script>
