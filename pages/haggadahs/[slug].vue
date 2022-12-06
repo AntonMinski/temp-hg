641eb6020a214908289c5617c65d64471996cc2f
Anton Lipskiy <anton.lipskiy@digiscorp.com>
2022-12-06 14:59:57 +0000

    Feature/100 other haggadahs by contributor

--- pages/haggadahs/[slug].vue
+++ pages/haggadahs/[slug].vue
@@ -5,26 +5,43 @@
       <UIContainer>
         <div class="flex justify-between">
           <div class="flex items-center">
-            <div class="mr-3">
-              <img :src="Avatar" alt="Avatar" class="h-12 w-12" />
-            </div>
-            <div class="flex flex-col gap-0.5">
-              <span class="text-xs text-white">Contributed by</span>
-              <UIHeading :level="6" class="!text-white">Esther Kustanowitz</UIHeading>
-              <span class="text-xs text-white">212 Followers • 12 Haggadahs • 26 Clips</span>
+            <div class="ml-2 flex flex-col gap-0.5">
+              <BlockContributor
+                :initials="contributor.author_initials"
+                :name="contributor.author"
+                text="Contributed by"
+                size="lg"
+                class="contributor-details !mt-0">
+                <template #heading>
+                  <span class="text-xs text-white">Contributed by</span>
+                  <UIHeading :level="6" class="mb-2 !text-white">{{ contributor.author }}</UIHeading>
+                </template>
+                <template #details>
+                  <span class="block text-xs text-gray-600 dark:text-gray-400">
+                    {{ contributor.total_followers }} Followers • {{ contributor.total_books }} Haggadahs •
+                    {{ contributor.total_clips }} Clips
+                  </span>
+                </template>
+              </BlockContributor>
             </div>
           </div>
           <div class="flex w-[411px] items-center">
-            <UIButton class="mr-4 w-full bg-accent-yellow-500 text-gray-900 hover:bg-accent-yellow-600">
+            <UIButton
+              class="mr-4 w-full bg-accent-yellow-500 text-gray-900 hover:bg-accent-yellow-600"
+              @click="followContributor">
               Follow Contributor
             </UIButton>
-            <UIButton color="light" class="w-full border bg-transparent text-white hover:bg-white hover:text-gray-900">
+            <UIButton
+              color="light"
+              class="w-full border bg-transparent text-white hover:bg-white hover:text-gray-900"
+              @click="viewProfile">
               View Profile
             </UIButton>
           </div>
         </div>
       </UIContainer>
     </div>
+    <ExploreHaggadahsPreviewByAuthor :contributor="contributor" />
   </div>
 </template>
 
@@ -35,6 +52,8 @@ import { ref, Ref } from 'vue';
 import { Contributor } from '~/components/Global/Contributor/types';
 import { getMetaObject } from '~/composables/meta';
 import { useHead } from '#head';
+import { loginModal } from '~/composables/loginModal';
+import { loginRedirect } from '~/composables/loginRedirect';
 
 const Avatar = (await import('@/assets/images/avatar.png')).default;
 const route = useRoute();
@@ -54,9 +73,33 @@ const contributor: Ref<Contributor> = ref(initialData?.contributed_by);
 const similarTopicHaggadah: Ref<Haggadah> = ref(initialData?.similar_topic_haggadah);
 const meta_tags = ref(initialData?.meta_tags);
 
+async function followContributor() {
+  const response = await vueApp.$api.contributor.followContributor(contributor.value.id);
+  // check if user is logged in
+  if (loginModal()) {
+    return;
+  }
+  // api not implemented yet
+}
+
+async function viewProfile() {
+  // check if admin is authenticated
+  if (await loginRedirect()) {
+    return;
+  }
+  // api not implemented yet
+}
+
 const metaObject = getMetaObject(meta_tags);
 useHead({
   title: meta_tags?.title,
   meta: metaObject,
 });
 </script>
+
+<style>
+.contributor-details,
+.contributor-details * {
+  @apply text-white;
+}
+</style>
