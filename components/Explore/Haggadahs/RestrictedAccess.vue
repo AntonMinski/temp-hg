eec5ae0808edd1f0faadce85c90d4e71cb86061c
Karen Munoz <karen00munoz@gmail.com>
2022-12-07 18:29:11 +0500

    complete task #57

---
+++ components/Explore/Haggadahs/RestrictedAccess.vue
@@ -0,0 +1,18 @@
+<template>
+  <UIContainer>
+    <div class="mt-[105px] mb-[95px] flex flex-col items-center justify-center py-[115px]">
+      <img :src="restrictedAccessIcon" alt="Restricted access icon" class="h-auto w-[180px]" />
+      <UIHeading :level="3" class="mt-[39px] !text-3xl !leading-[45px] text-tertiary-600">
+        Access restricted for this Haggadah
+      </UIHeading>
+      <p class="mt-[5px] text-lg text-gray-800">This Haggadah is private and require access from the contributor.</p>
+      <div class="mt-[39px]">
+        <UIButton color="gray" class="!leading-[27px] hover:bg-gray-700">Request access</UIButton>
+      </div>
+    </div>
+  </UIContainer>
+</template>
+
+<script lang="ts" setup>
+const restrictedAccessIcon = (await import('@/assets/svg/Eye.svg')).default;
+</script>
