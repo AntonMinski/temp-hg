c539ec16ff6c25b3df6db6229a23a68912aa78a5
Karen Munoz <karen00munoz@gmail.com>
2022-12-13 04:24:49 +0500

    adding changes

--- pages/signin/index.vue
+++ pages/signin/index.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="bg-tertiary-500 pt-[120px] pb-36">
+  <div class="relative bg-tertiary-500 pt-[120px] pb-36">
+    <img :src="signinScreenIcon" class="invisible absolute left-[80px] bottom-[73px] max-w-[126px] lg:visible" />
     <FormsLogin />
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const signinScreenIcon = (await import('@/assets/svg/login-screen-icon.svg')).default;
+</script>
